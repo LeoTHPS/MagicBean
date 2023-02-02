37ab04e709e954cd56e4e901edfaa88858b7798d
Francis Barney <leo633395@gmail.com>
2023-02-02 14:13:58 -0800

    update

--- Samples/Lua/test.lua
+++ Samples/Lua/test.lua
@@ -24,13 +24,19 @@ if not magic_bean_is_null(process) then
 		end
 	);
 
+	io.write("Enable console? y/n: ");
+	local isConsoleEnabled = io.read("l") == "y";
+	magic_bean_process_memory_write_uint32(process, 0x00CABCC4, isConsoleEnabled and 1 or 0);
+	print("Console " .. (isConsoleEnabled and "enabled" or "disabled"));
+
 	local playerGUID = magic_bean_process_memory_read_uint64(process, 0x00CA1238);
 
 	if playerGUID ~= 0 then
-		local realmName  = magic_bean_process_memory_read_string(process, 0x00C79B9E, 32);
-		local playerName = magic_bean_process_memory_read_string(process, 0x00C79D18, 16);
+		local realmName   = magic_bean_process_memory_read_string(process, 0x00C79B9E, 32);
+		local playerName  = magic_bean_process_memory_read_string(process, 0x00C79D18, 16);
+		local playerIsAfk = magic_bean_process_memory_read_uint32(process, 0x00BCEFEC) ~= 0;
 
-		print(playerName .. ":" .. string.format("%x", playerGUID) .. " @ " .. realmName);
+		print((playerIsAfk and "<away> " or "") .. playerName .. ":" .. string.format("%x", playerGUID) .. " @ " .. realmName);
 	end
 
 	magic_bean_process_close(process);
